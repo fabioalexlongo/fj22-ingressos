0f1c8c68b6f9e7907527544b5fbe46c1cbf4d5ec
fabioalexlongo <fabioalexlongo@gmail.com>
2021-05-05 21:42:13 -0300

    4.4 EXERCÍCIO - CONSUMINDO SERVIÇO PARA DETALHES DO FILME

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -1,5 +1,24 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+
+import br.com.caelum.ingresso.model.desconto.Desconto;
+
 public class Ingresso {
 
+	private Sessao sessao;
+	private BigDecimal preco;
+	
+	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+		this.sessao = sessao;
+		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 }
